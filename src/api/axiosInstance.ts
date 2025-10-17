aef16e8ca87e20a220609f27e8db65f1a9f1e2cc
haidang2312 <danghai23122005@gmail.com>
2025-10-17 08:53:54 +0700

    Update axiosInstance.ts

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -10,7 +10,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token"); 
     if (token) {
-        config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1X3JvbGVzIjpbIlJPTEVfQURNSU4iXSwidV9pZCI6MiwidV9zdG9yZV9uYW1lIjoiTmjDoCBow6BuZyBBIC0gUXXhuq1uIDEiLCJ0b2tlbl90eXBlIjoiYWNjZXNzX3Rva2VuIiwic3ViIjoiY29ubG9uYmVvMSIsImlhdCI6MTc2MDY2MjE0MCwiZXhwIjoxNzYwNjY1NzQwfQ.kLFDLdNti15S3Q2n_A_XbI9o5JtIVGAIxAIIw0mrzX0`;  
+        config.headers.Authorization = `Bearer ${token}`;  
     }
     return config;
 });
